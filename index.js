144437e2524c4ee8e427f1f5c10d8e77f57b5f68
Wallace Oliveira <wallacebatistaoliveira@gmail.com>
2021-10-08 10:21:29 -0300

    adiciona select e lista de pessoas

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
 const express = require('express')
+const mysql = require('mysql2/promise')
 
 const app = express()
 const port = 3000
 
-app.get('/', (request, response) => {
-  response.send(`<h1>Full Cycle Rocks!</h1>`)
+const config = {
+  host: 'database',
+  user: 'root',
+  database: 'nodedb',
+  password: 'root'
+}
+
+const connection = mysql.createConnection(config)
+
+async function fetchPeople() {
+  const [results] = await (await connection).query(`select * from people`)
+  return results
+}
+
+app.get('/', async (request, response) => {
+  const people = await fetchPeople() || []
+
+  response.send(`
+    <h1>Full Cycle Rocks!</h1>
+    ${people.length > 0 ? `
+      <ul>
+        ${people.map(person => (
+          `<li>${person.name}</li>`
+        ))}
+      </ul>
+    ` : ''}
+  `)
 })
 
 app.listen(port, () => {
