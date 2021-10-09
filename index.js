4755c2477394685cf7350ec8470a1426f9300c46
Wallace Oliveira <wallacebatistaoliveira@gmail.com>
2021-10-09 12:54:57 -0300

    adiciona insersão de pessoas no banco

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mysql = require('mysql2/promise')
+const faker = require('faker')
 
 const app = express()
 const port = 3000
@@ -18,7 +19,12 @@ async function fetchPeople() {
   return results
 }
 
+async function createPerson() {
+  await (await connection).query(`insert into people(name) values('${faker.name.findName()}')`)
+}
+
 app.get('/', async (request, response) => {
+  await createPerson()
   const people = await fetchPeople() || []
 
   response.send(`
@@ -27,7 +33,7 @@ app.get('/', async (request, response) => {
       <ul>
         ${people.map(person => (
           `<li>${person.name}</li>`
-        ))}
+        )).join('')}
       </ul>
     ` : ''}
   `)
